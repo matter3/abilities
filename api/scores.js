88d93420aed9f5d86ec0b2a85a1a600dcbc40ac9
Matt Elliott <matter3@gmail.com>
2021-09-03 06:30:37 -0600

    Initial commit

---
+++ api/scores.js
@@ -0,0 +1,45 @@
+import { utils as etherUtils, BigNumber } from 'ethers';
+import { flatten, range, orderBy } from 'lodash';
+import pMap from 'p-map';
+import ranks from '~/static/ranks.js';
+
+const offset = range(0, 500, 50);
+const fetchScoreSet = (offset) => {
+  return fetch(`https://api.opensea.io/api/v1/assets?collection=ability-score&offset=${offset}&limit=50`)
+  .then(async response => {
+    return await response.json().then(json => {
+      return prune(json.assets);
+    })
+  });
+}
+
+const fetchAllScores = async () => {
+  const allSets = await pMap(offset, fetchScoreSet, { concurrency: 2 });
+  return orderBy(flatten(allSets).map(set => {
+    const match = ranks.items.filter(item => item.tid === Number.parseInt(set.token_id))[0];
+    return {
+      ...set,
+      ...match,
+      price: getPrice(set),
+    }
+  }), 'rank');
+}
+
+const prune = (assets) => {
+  return assets.filter(asset => {
+    return !!asset.image_url && !!asset.sell_orders;
+  });
+}
+
+const getPrice = (asset) => {
+  return Number(
+    etherUtils.formatUnits(
+      BigNumber.from(asset.sell_orders[0]?.current_price.split('.')[0]),
+    ),
+  );
+}
+
+export {
+  fetchAllScores,
+  getPrice
+}
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="home">
+    <h1 class="home__header">Ability Scores</h1>
+    <p class="home__intro">Displays Ability Scores for sale by lowest price on <a href="https://opensea.io/collection/ability-score">OpenSea</a>. Includes rarity rank which is calculated <a href="https://github.com/Anish-Agnihotri/dhof-loot/blob/master/derivatives/andy8052-ability-score/output/score.json">here</a>.</p>
+    <div class="home__ability-cards">
+      <div v-for="item in items" :key="item.id" class="home__ability-cards__card">
+        <img :src="item.image_url">
+        <a :href="item.permalink">View on Opensea</a>
+        <span>Rank: {{ item.rank }} Score: {{ item.score }} Price {{ item.price }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fetchAllScores } from '~/api/scores.js';
+
+export default {
+  data() {
+    return {
+      items: [],
+    }
+  },
+  async fetch() {
+    this.items = await fetchAllScores();
+  },
+  methods: {
+    collectData() {
+
+    },
+    getColor(tokenId) {
+      
+    },
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  max-width: 1096px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.home__intro {
+  font-size: 1.2rem;
+  text-align: center;
+}
+
+.home__header {
+  text-align: center;
+  font-size: 5rem;
+}
+.home__ability-cards .home__ability-cards__card {
+  display: inline-block;
+  padding: 3rem;
+  border: 3px solid #fff;
+  margin: 1rem;
+  width: 20%;
+}
+</style>
